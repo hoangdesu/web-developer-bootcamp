811999845142d06aeff4bf8493cfd126d68fc56f
Hoang Nguyen <hoangdesu@gmail.com>
2021-04-07 13:51:51 +0700

    9. CSS properties

---
+++ 9_CSS properties/Hover_Button/app.js
@@ -0,0 +1,8 @@
+let btnClickMe = document.querySelector("button");
+
+// Arrow function hihi
+const displayMsg = () => {
+    alert("You're the best, have a nice day! ❤️");
+}
+
+btnClickMe.addEventListener("click", displayMsg)
