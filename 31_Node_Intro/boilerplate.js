2dbbf11937d7f67136456a3cfc1a7a2bd8c9bc8b
hoangdesu <hoangdesu@gmail.com>
2021-10-19 15:45:41 +0700

    31. Node done

---
+++ 31_Node_Intro/boilerplate.js
@@ -0,0 +1,37 @@
+// file system module
+
+const fs = require('fs');
+const process = require('process');
+
+// console.log(fs);
+
+// -> async version - bit more complicated but doesn't block other processes
+// fs.mkdir('./Project', { recursive: true }, err => {
+//     if (err) throw err;
+//     console.log('Folder "Project" has been created');
+// });
+
+// -> sync version - simple, but blocks everything if heavy
+// fs.mkdirSync('Project');
+
+// console.log('This line comes after mkdir');
+
+// -- Code to create a boilerplate project folder
+let folder = process.argv[2] || 'unnamed';
+folder += ' (auto generated)';
+// remove if folder exists
+if (fs.existsSync(folder)) {
+    fs.rmdirSync(folder, { recursive: true });
+    console.log('Old folder has been removed');
+}
+fs.mkdirSync(`${folder}`);
+
+try {
+    fs.writeFileSync(`./${folder}/index.html`, '');
+    fs.writeFileSync(`./${folder}/app.js`, '');
+    fs.writeFileSync(`./${folder}/style.css`, '');
+} catch (e) {
+    console.log(e);
+}
+
+console.log(`"${folder}" folder has been created!`);
