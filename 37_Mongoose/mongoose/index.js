cd45c7da8e494c06de56f615a530a71441c44e2e
Hoang Nguyen <hoangdesu@gmail.com>
2023-03-03 04:14:11 +0700

    388. insert many + reading on queries, promises and exec()

--- 37_Mongoose/mongoose/index.js
+++ 37_Mongoose/mongoose/index.js
@@ -30,8 +30,13 @@ async function main() {
     
     // models
     const Movie = mongoose.model('Movie', movieSchema); // will be turned into "movies" for collection name
-    const Actor = mongoose.model('Actor', movieSchema);
+    const Actor = mongoose.model('Actor', actorSchema);
     
+
+    // reset db
+    await Movie.deleteMany({});
+    await Actor.deleteMany({});
+
 
     // create actual movie instances (documents)
     const m1 = new Movie({ title: 'Alice in Borderland', year: 2020, score: 7.7, rating: 'R', watched: true });
@@ -43,11 +48,40 @@ async function main() {
     
     
     const m2 = new Movie({ title: 'Spy x Family' });
-    await m2.save();
+    // await m2.save();
 
 
-    const allMovies = await Movie.find();
-    console.log(allMovies);
+    const allMovies = await Movie.find({}).exec();
+    console.log('all movies:', allMovies);
+
+    // - insert many = create the docs and save directly to db
+    if (true) {
+        const insertManyRes = await Movie.insertMany([
+            { title: 'Arcane', score: 9.0, watched: true },
+            { title: 'Friends', score: 8.9 },
+            { title: 'Lupin', score: 7.5, watched: true },
+            { title: 'Mr.Robot', score: 8.5, watched: false },
+            { title: 'Avatar 2', score: 7.8, watched: true },
+        ]);
+    }
+    // console.log('insertmany result', insertManyRes);
+
+    if (false) {
+        Actor.insertMany([
+            { name: 'Brian' },
+            { name: 'Cun' },
+            { name: 'Hoang' },
+        ]).then(actor => {
+            console.log('saved actor: ', actor);
+        }).catch(err => {
+            console.error(err);
+        });
+    }
+
+
+    // FIND
+    // find movies with score >= 8.0
+    Movie.find({score: { $gte: 8.0 }}).exec().then(m => console.log('found:', m));
 
 }
 
