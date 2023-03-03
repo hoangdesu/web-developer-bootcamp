03264dcaf51ed13cc7e2aae520643d2281be934f
Hoang Nguyen <hoangdesu@gmail.com>
2023-03-04 02:42:13 +0700

    389. different find methods and uses in mongoose

--- 37_Mongoose/mongoose/index.js
+++ 37_Mongoose/mongoose/index.js
@@ -57,16 +57,22 @@ async function main() {
     // - insert many = create the docs and save directly to db
     if (true) {
         const insertManyRes = await Movie.insertMany([
+            m1,
+            m2,
             { title: 'Arcane', score: 9.0, watched: true },
             { title: 'Friends', score: 8.9 },
             { title: 'Lupin', score: 7.5, watched: true },
-            { title: 'Mr.Robot', score: 8.5, watched: false },
+            { title: 'Mr. Robot', score: 8.5, watched: false },
             { title: 'Avatar 2', score: 7.8, watched: true },
+            { title: 'Avatar', score: 8.1, watched: true },
+            { title: 'Now you see me', score: 8.5, watched: true },
+            { title: 'John Wick', score: 7.9, watched: false },
+            { title: 'Johnny English', score: 7.1, watched: false },
         ]);
     }
     // console.log('insertmany result', insertManyRes);
 
-    if (false) {
+    if (true) {
         Actor.insertMany([
             { name: 'Brian' },
             { name: 'Cun' },
@@ -81,8 +87,26 @@ async function main() {
 
     // FIND
     // find movies with score >= 8.0
-    Movie.find({score: { $gte: 8.0 }}).exec().then(m => console.log('found:', m));
+    Movie.find({ score: { $gte: 8.0 } }).exec().then(movies => console.log('movies with score >= 8:', movies));
 
+    // find movies with score >= 7.5 that have not been watched, selecting only "title" and "score" fields. Using ['title', 'score'] is also OK
+    Movie.find({ watched: false, score: { $gt: 7.5 } }, 'title score').then(movies => console.log('unwatched movies with score >= 7.5:', movies));
+
+    // find movies that has "john" in their title using regex
+    Movie.find({ title: /john/i }).then(m => console.log('movies with "john" in their title:', m));
+
+    // find the first matching "Ava" using async/await
+    const firstMatchAva = await Movie.findOne({ title: /Ava/ }).exec();
+    console.log('First matching "Ava":', firstMatchAva);
+
+    // using callback
+    Movie.find({ watched: true }, 'title score', function(err, movies) {
+        if (err) console.log(err);
+        console.log('watched movies:', movies);
+    });
+
+    // find by ID
+    Movie.findById(m1._id).then(m => console.log('movie 1 find by id:', m));   
 }
 
 
