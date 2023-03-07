8cf17d156440c74176c13f3d43b7f6b22edfda8f
Hoang Nguyen <hoangdesu@gmail.com>
2023-03-08 03:16:55 +0700

    395. validations, constraints, errors, validate updates

--- 37_Mongoose/mongoose/product.js
+++ 37_Mongoose/mongoose/product.js
@@ -21,27 +21,59 @@ async function main() {
     name: {
       type: String,
       required: true,
+      minLength: 6,
+      
+      // -- testing constraints
+      // maxLength: 5
+      // enum: ['iPhone', 'MacBook']
+      // lowercase: true,
+      // uppercase: true,
+      // immutable: true,
+      // match: /iphone/i
+      // enum: { values: ['Coffee', 'Tea'], message: '{VALUE} is not supported' }
     },
     price: {
       type: Number,
       required: true,
+      min: [0, 'Price cannot be negative'],
+      // max: [10, 'Value {VALUE} must be less than 10']
     },
     onSale: {
       type: Boolean,
       default: false
     },
+    qty: {
+      online: {
+        type: Number,
+        default: 0
+      },
+      inStore: {
+        type: Number,
+        default: 0
+      }
+    },
+    model: {
+      type: String,
+      enum: ['Mini', 'Base', 'Pro', 'Pro Max'],
+      default: 'Pro'
+    }
   });
 
   // model == collection
   const Product = mongoose.model("Product", productSchema); // db in mongo: "products"
 
   // drop table
-  await Product.deleteMany({});
+  // await Product.deleteMany({});
 
   const iPhone = new Product({
     name: "iPhone 12 Pro",
     price: 16000000,
     color: "black", // not defined in schema, won't be saved to db
+    onSale: true,
+    qty: {
+      online: 2,
+      inStore: 3
+    },
   });
 
   iPhone
@@ -49,9 +81,14 @@ async function main() {
     .then((data) => console.log("saved iphone:", data))
     .catch((err) => console.error("Error:", err));
 
-  Product.find({}).then((p) => console.log("- all products:", p));
-}
 
+  Product.find({}).then((p) => console.log("- all products:", p));
+
+  // validating updates
+  Product.findOneAndUpdate({ name: 'iPhone 12 Pro' }, { price: 26.02 }, { new: true, runValidators: true })
+    .then(data => console.log('new data', data)); // error, price < 0
+
+}
 
 
 // node REPL
