096314800889cb5a1fc16ab807196b3263f0c551
Hoang Nguyen <hoangdesu@gmail.com>
2023-03-10 02:41:29 +0700

    using axios from svelte client

--- 38_Mongoose_Express/client/src/App.svelte
+++ 38_Mongoose_Express/client/src/App.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
   import { onMount } from 'svelte';
+  import axios from 'axios';
   let foodList;
+  const URL = 'http://localhost:3001/findAll';
 
   onMount(async () => {
-        const response = await fetch(
-            'http://localhost:3001/findAll',
-            {
-                method: 'GET',
-            }
-        );
-        foodList = await response.json();
+        // const response = await fetch(
+        //     'http://localhost:3001/findAll',
+        //     {
+        //         method: 'GET',
+        //     }
+        // );
+        // foodList = await response.json();
+        
+        const response = await axios.get(URL);
+        foodList = response.data;
         console.log('food list:', foodList);
       });
 </script>
