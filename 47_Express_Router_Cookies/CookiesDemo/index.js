26ab333f54eebbf96a2816e1f194a62aa5e624f8
Hoang Nguyen <hoangdesu@gmail.com>
2023-07-21 18:38:12 +0700

    490. signing cookies

--- 47_Express_Router_Cookies/CookiesDemo/index.js
+++ 47_Express_Router_Cookies/CookiesDemo/index.js
@@ -3,18 +3,35 @@ const app = express();
 const cookieParser = require('cookie-parser');
 const PORT = 3000;
 
-app.use(cookieParser());
+// sign cookie with a secret
+app.use(cookieParser('secret')); // s%3Aprogramming.PbN9UdvJHJGquwCSxKMMqxWAY1plAX%2BhI3rkKIrLKfc
+// app.use(cookieParser('idontknow')); // s%3Aprogramming.7lDeo2nJcrUBwBe74am4lEcJU5%2Bu2X4tndgPqSYrCzk
 
-app.get('/hi', (req, res) => {
+app.get('/setcookies', (req, res) => {
     const { name } = req.query;
     if (name) {
         res.cookie('name', name);
-        res.send(`Hi ${name}`)
+        res.send(`${name} just got a cookie!`)
     } else {
-        res.send('Hi world!');
+        res.send('No cookie for you :(');
     }
 });
 
+app.get('/hi', (req, res) => {
+    console.log(req.cookies);
+    const { name } = req.cookies;
+    name ? res.send(`Hi ${name}`) : res.send('Cookies name not set :(');
+});
+    
+app.get('/signcookie', (req, res) => {
+    // set signed option
+    res.cookie('love', 'programming', { signed: true });
+    res.send('Signed a cookie');
+});
+
+app.get('/getsignedcookies', (req, res) => {
+    res.send(req.signedCookies);
+});
 
 app.listen(PORT, () => {
     console.log(`🚀 Server running at http://localhost:${PORT}`);
